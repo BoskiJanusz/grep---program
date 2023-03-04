c258881361ff815cc0135c0f278df7791e388999
BoskiJanusz <patmich@onet.eu>
2023-03-04 18:33:04 +0100

    todo comments

--- main.cpp
+++ main.cpp
@@ -17,7 +17,30 @@ void wordsFinder(const std::string filePath, const std::string  word)
     }
 }
 
+/*todo
+change program name
+add directories and files for program 
+*/
 
+
+/*todo
+result txt file 
+*/
+
+/*todo
+result log file 
+*/
+
+
+/* todo 
+parameter handling
+ -d || --dir - start directory where program needs to look for files (also in subfolders)
+l || --log_file - name of the log file
+-r || --result_file - name of the file where result is given
+-t || --threads - number of threads in the pool
+
+no parameter handling
+*/
 int main()
 {
     fs::path defultPath = fs::current_path();
