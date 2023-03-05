e3ee443518ad9eeb5798dff6c704c7ab557aa21d
BoskiJanusz <patmich@onet.eu>
2023-03-05 15:42:56 +0100

    parameter handling
    new program name
    
    new library boost option_program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <algorithm>
-#include <iostream>
+#include <boost/program_options.hpp>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 namespace fs = std::filesystem;
+namespace po = boost::program_options;
 
 void wordsFinder(const std::string filePath, const std::string  word)
 {
@@ -12,44 +14,77 @@ void wordsFinder(const std::string filePath, const std::string  word)
     std::string line;
     while(getline(istrm, line))
     {
-        if (line.find(word) != std::string::npos)
-            std::cout << filePath << std::endl;
+      if (line.find(word) != std::string::npos) {
+        std::cout << filePath << std::endl;
+      }
     }
 }
 
 /*todo
-change program name
-add directories and files for program 
-*/
-
-
-/*todo
-result txt file 
+add directories and files for program
 */
 
 /*todo
-result log file 
+function result txt file
 */
 
-
-/* todo 
-parameter handling
- -d || --dir - start directory where program needs to look for files (also in subfolders)
-l || --log_file - name of the log file
--r || --result_file - name of the file where result is given
--t || --threads - number of threads in the pool
-
-no parameter handling
+/*todo
+function result log file
 */
-int main()
-{
-    fs::path defultPath = fs::current_path();
-    std::string word = "words";
-    for (auto const& directories : fs::recursive_directory_iterator{defultPath}) 
-    {
-       wordsFinder(directories.path(), word); 
+
+/*todo
+function dir path
+*/
+
+int main(int argc, char **argv) {
+  std::string word, directionPath, logFileName, resultsFileName;
+  int numberOfThreads;
+
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "print usage message")("input", po::value(&word),
+                                                      "input characters")(
+      "dir,d", po::value(&directionPath)->default_value(fs::current_path()),
+      "start directory where program needs to look for files")(
+      "log_file,l", po::value(&logFileName)->default_value("specific_grep.log"),
+      "name of the log file")(
+      "result_file,r",
+      po::value(&resultsFileName)->default_value("specific_grep.txt"),
+      "name of the file where result is given")(
+      "threads,t", po::value(&numberOfThreads)->default_value(4),
+      "number of threads in the pool");
+
+  po::positional_options_description positionals;
+  positionals.add("input", -1);
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv)
+                .positional(positionals)
+                .options(desc)
+                .run(),
+            vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cerr << desc << "\n";
+    return 1;
+  }
+
+  if (vm.count("input")) {
+    if (vm.count("dir")) {
+      for (auto const &directories :
+           fs::recursive_directory_iterator{directionPath}) {
+        wordsFinder(directories.path(), word);
+      }
     }
- 
-   
-    return 0;
+    if (vm.count("log_file")) {
+      /*todo*/
+    }
+    if (vm.count("result_file")) {
+      /*todo*/
+    }
+    if (vm.count("threads")) {
+      /*todo*/
+    }
+  }
+
+  return 0;
 }
